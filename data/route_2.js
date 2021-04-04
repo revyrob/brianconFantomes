b53d22431900b45a2b1f56f8c8de04b56651b676
revyrob <kayle.robson@gmail.com>
2021-04-04 17:06:09 +0200

    first commit

---
+++ data/route_2.js
@@ -0,0 +1,16 @@
+var json_route_2 = {
+"type": "FeatureCollection",
+"name": "route_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Stop": "1", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.643974711667213, 44.901183152617598 ] } },
+{ "type": "Feature", "properties": { "Stop": "2", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.644345750463843, 44.899323698269782 ] } },
+{ "type": "Feature", "properties": { "Stop": "3", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.645894837439768, 44.898094979891127 ] } },
+{ "type": "Feature", "properties": { "Stop": "4", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.644716789260471, 44.898035843050216 ] } },
+{ "type": "Feature", "properties": { "Stop": "5", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.643548017051089, 44.898554931022105 ] } },
+{ "type": "Feature", "properties": { "Stop": "6", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.643084218555304, 44.898870323956899 ] } },
+{ "type": "Feature", "properties": { "Stop": "7", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.642750283638337, 44.899698222179708 ] } },
+{ "type": "Feature", "properties": { "Stop": "8", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.642416348721371, 44.899704792752843 ] } },
+{ "type": "Feature", "properties": { "Stop": "9", "Photo": "\/photos\/stop1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 6.643381049592606, 44.899369692565557 ] } }
+]
+}
